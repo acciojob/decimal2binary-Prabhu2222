646bee555335405babec8cb6b0e67a1ddb9fa5ce
Prabhu2222 <120732312+Prabhu2222@users.noreply.github.com>
2023-09-11 12:54:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function decimalToBinary(num) {
   //Write you code here
+	let arr=[];
+	while(num>0){
+		let rem=num%2;
+		let quo=Math.round(num/2);
+    console.log(quo,rem);
+    
+		arr.unshift(rem);
+		num=quo;
+	}
+  // console.log(arr);
+	let strnum=arr.join("");
+	let number=Number(strnum);
+	return number;
   
 }
 
